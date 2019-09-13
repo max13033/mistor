3abdf04912e92bef90e40b47ad3f1f384b9108f3
max1303 <max1303@rambler.ru>
2019-09-13 13:55:59 +0300

    sincronisation

--- connect.php
+++ connect.php
@@ -1,7 +1,9 @@
 <?php
 error_reporting(E_ALL);
 
-$connect = new mysqli("localhost", "max1303_mistor", "362718aB", "max1303_mistor");
+// $connect = new mysqli("localhost", "max1303_mistor", "362718aB", "max1303_mistor");
+
+$connect = new mysqli("localhost", "root", "mysql", "mistor");
 
 if (mysqli_connect_errno()) {
     echo mysqli_connect_error();
--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 
 //include('/home/m/max1303/public_html/mistor.su/pages/site.php');
-header("location: https://mistor.su/main/");
+header("location: main/");
 // echo $_SERVER['DOCUMENT_ROOT'].'/mistor.su/pages/site.php';
 
 // /home/m/max1303/public_html
